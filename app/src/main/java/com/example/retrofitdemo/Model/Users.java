ef3f77a603f353edf873d07774f4b1236a4799a0
Manali Modi <modimanali231@gmail.com>
2021-03-09 10:15:28 +0530

    new file

---
+++ app/src/main/java/com/example/retrofitdemo/Model/Users.java
@@ -0,0 +1,20 @@
+package com.example.retrofitdemo.Model;
+
+import java.util.List;
+
+public class Users {
+
+    private List<Data> data;
+
+    public Users(List<Data> data) {
+        this.data = data;
+    }
+
+    public List<Data> getData() {
+        return data;
+    }
+
+    public void setData(List<Data> data) {
+        this.data = data;
+    }
+}
