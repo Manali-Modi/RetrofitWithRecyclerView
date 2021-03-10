19a36118bb0b62c0143e22cd85133b52c926e803
Manali Modi <modimanali231@gmail.com>
2021-03-10 14:30:10 +0530

    change limit to dynamic

--- app/src/main/java/com/example/retrofitdemo/model/Users.java
+++ app/src/main/java/com/example/retrofitdemo/model/Users.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class Users {
 
     private List<Data> data;
+    private int total_pages;
 
     public Users(List<Data> data) {
         this.data = data;
@@ -13,7 +14,16 @@ public class Users {
     public List<Data> getData() {
         return data;
     }
+
     public void setData(List<Data> data) {
         this.data = data;
+    }
+
+    public int getTotal_pages() {
+        return total_pages;
+    }
+
+    public void setTotal_pages(int total_pages) {
+        this.total_pages = total_pages;
     }
 }
